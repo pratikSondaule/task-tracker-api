54a171927aabf809847fe8eeaceb268c5005176c
pratikSondaule <pratiksondaule@gmail.com>
2025-11-15 22:02:54 +0530

    working on inviting collaborator

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,14 +1,17 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { TaskStatus } from '@prisma/client';
 import { PrismaService } from 'src/services/prisma.service';
+import { SendMailService } from 'src/services/sendMail.service';
 import { CreateTaskDto } from 'src/validations/task/createTask.dto';
+import { InviteCollaboratorDto } from 'src/validations/task/inviteCollaborator.dto';
 import { UpdateTaskDto } from 'src/validations/task/updateTask.dto';
 
 @Injectable()
 export class TaskService {
 
     constructor(
-        private prisma: PrismaService
+        private prisma: PrismaService,
+        private sendMail: SendMailService
     ) { }
 
     async createTask(request: any, data: CreateTaskDto) {
@@ -220,6 +223,77 @@ export class TaskService {
             console.log(error)
             throw new HttpException('Something went wrong while deleting task', HttpStatus.INTERNAL_SERVER_ERROR)
         }
+    }
+
+
+    async inviteCollaborator(request: any, task_id: string, data: InviteCollaboratorDto) {
+
+        console.log("User ", request.user)
+
+        const { id: loggedInUserId } = request.user
+
+        const user = await this.prisma.user.findUnique({
+            where: {
+                id: loggedInUserId
+            }
+        })
+
+        if (!user) {
+            throw new HttpException('User not found', HttpStatus.NOT_FOUND)
+        }
+
+        const getTaskById = await this.prisma.task.findUnique({
+            where: {
+                id: task_id
+            }
+        })
+
+        if (!getTaskById) {
+            throw new HttpException('Task not found', HttpStatus.NOT_FOUND)
+        }
+
+        try {
+
+            const mailOptions = {
+                from: process.env.SMTP_EMAIL,
+                to: data?.email,
+                subject: `Task Tracker Invitation`,
+                html: `
+                    <html>
+                        <body>
+                            <h2>Hello</h2>
+                            <p><b>${user.first_name} ${user.last_name}</b> has invited you to collaborate in a task <b>${getTaskById.title}</b> click on the link below to sign up on the platform</p>
+                            <p><a href='http://localhost:3000/docs#/Users/UserController_createUser'><b>SIGN UP</b></a></p>
+                        </body>
+                    </html>
+                `
+            }
+
+            const sendMail = await this.sendMail.sendMail(mailOptions)
+
+            if (sendMail.statusCode == 200) {
+
+                const collaboratorData = {
+                    task_id: getTaskById.id,
+                    email: data?.email,
+                    invited_by_id: user.id
+                }
+
+                await this.prisma.invite_collaborator.create({
+                    data: collaboratorData
+                })
+
+                return {
+                    status: HttpStatus.OK,
+                    message: "Invitation mail sent successfully"
+                }
+            }
+
+        } catch (error) {
+            console.log(error)
+            throw new HttpException('Something went wrong while inviting a collaborator', HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+
     }
 
 }
